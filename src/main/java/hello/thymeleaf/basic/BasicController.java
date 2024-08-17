9351eef6bb10dbad58fbf3b4d2762e9c538a6664
aammddkkzxc <ideal0427@naver.com>
2024-08-17 22:14:27 +0900

    feat: 속성 값 설정 th:* 속성을 지정하면 타임리프는 기존 속성을 th:* 로 지정한 속성으로 대체한다. 기존 속성이 없다면 새로 만든다.

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -91,6 +91,10 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
 
     @Data
     static class User {
